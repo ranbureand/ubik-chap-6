e08dcb3698d8d4696a431d7c1bd2316a7a17422d
Andrea Buran <ranbureand@users.noreply.github.com>
2015-11-21 23:37:23 +0200

    Improve the fading in of the pieces.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -17,29 +17,23 @@ $(document).ready(function() {
 
   function piece(pId, pFile, pWidth, pHeight, pX, pY) {
     $('.pieces').append('<video class="piece" id="l'+ pId +'" width="'+ pWidth +'" height="'+ pHeight +'" style="top: '+ pY +'px; left: '+ pX +'px;" preload="auto" poster="media/posters/'+ pFile +'.jpg" loop><source src="media/videos/'+ pFile +'.webm" type="video/webm"/><source src="media/videos/'+ pFile +'.mp4" type="video/mp4"/><source src="media/videos/'+ pFile +'.ogv" type="video/ogg"/></video>').hide().fadeIn(250);
-  }
+  };
+
+  function createPiece(pId, pFile, pWidth, pHeight, pX, pY) {
+    return (
+      $('<video class="piece" id="l'+ pId +'" width="'+ pWidth +'" height="'+ pHeight +'" style="top: '+ pY +'px; left: '+ pX +'px;" preload="auto" poster="media/posters/'+ pFile +'.jpg" loop><source src="media/videos/'+ pFile +'.webm" type="video/webm"/><source src="media/videos/'+ pFile +'.mp4" type="video/mp4"/><source src="media/videos/'+ pFile +'.ogv" type="video/ogg"/></video>')
+    );
+  };
+
+  var jPieces = [];
 
   for (var i = 0, j = pieces.length; i < j; i += 1) {
-    /*(function(i){
-
-        window.setTimeout(function(){
-          piece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY);
-        }, i * 100);
-
-    }(i));*/
-    piece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY);
+    jPieces.push(createPiece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY));
   }
 
-  /*
-  $(".faded").each(function(i) {
-    $(this).delay(i * 400).fadeIn();
-  });
-  */
+  //alert(jPieces.length);
 
-  $( '.piece' ).on( 'click', function() {
-    $(window).scrollTo($(this), 750, {offset:{left: -xpos, top: -ypos }, easing:"easeOutCubic"});
-    return false;
-  });
+  //$('.pieces').append(jPieces).delay(500).hide().fadeIn(400);
 
   (function($){
     $.fn.onPlayground = function() {
@@ -61,7 +55,67 @@ $(document).ready(function() {
     };
   })(jQuery);
 
-  setTimeout(function(){ $(".piece").onPlayground(); }, 251);
+  (function($){
+    $.fn.onPlayg = function() {
+
+      // Set the status of the play button icon
+      if($(this).is(':in-viewport')) {
+        $(this)[0].play();
+        console.log('play');
+      } else {
+        $(this)[0].pause();
+        console.log('pause');
+      }
+
+      return this;
+    };
+  })(jQuery);
+
+  $(jPieces).each(function(i, element) {
+    console.log(i);
+    $(element).appendTo( ".pieces" ).hide().delay((i+=1) * 50).fadeIn(400, function() {
+      $(element).onPlayg();
+    });
+    //$(element).onPlayg();
+  });
+
+  /*$('.pieces').append(jPieces.hide());
+
+  $( "<p>Test</p>" ).appendTo( ".inner" );*/
+
+  //$('.pieces').append(jPieces);
+
+  /*$(function() {
+    jPieces.each(function(i) {
+      $(this).delay((i++) * 200).fadeIn(400);
+    });
+  });*/
+
+  /*for (var i = 0, j = pieces.length; i < j; i += 1) {
+    (function(i){
+
+        window.setTimeout(function(){
+          piece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY);
+        }, i * 100);
+
+    }(i));
+    piece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY);
+  }*/
+
+  /*
+  $(".faded").each(function(i) {
+    $(this).delay(i * 400).fadeIn();
+  });
+  */
+
+  $('.piece').on( 'click', function() {
+    $(window).scrollTo($(this), 750, {offset:{left: -xpos, top: -ypos }, easing:"easeOutCubic"});
+    return false;
+  });
+
+  
+
+  //setTimeout(function(){ $(".piece").onPlayground(); }, (pieces.length*50)+1);
 
   $(window).on('scrollstop', function() {
     console.log('End of scrolling')
