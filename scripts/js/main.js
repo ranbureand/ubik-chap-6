2171e8e59841d95411de859fe0b168fa38aa4f2e
Andrea Buran <ranbureand@users.noreply.github.com>
2015-11-21 18:28:04 +0200

    Fade pieces in and play/pause them if in/not-in viewport.

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -4,6 +4,7 @@
 // @codekit-prepend "jquery.easing.1.3.js"
 // @codekit-prepend "jquery.scrollTo.js"
 // @codekit-prepend "isInViewport.js"
+// @codekit-prepend "jquery.scrollstop.js"
 // @codekit-prepend "pieces.js"
 
 $(document).ready(function() {
@@ -15,17 +16,75 @@ $(document).ready(function() {
   var ypos = alt / 100 * 20;
 
   function piece(pId, pFile, pWidth, pHeight, pX, pY) {
-    $('.pieces').append('<video class="piece" id="l'+ pId +'" width="'+ pWidth +'" height="'+ pHeight +'" style="top: '+ pY +'px; left: '+ pX +'px;" preload="auto" poster="media/posters/'+ pFile +'.jpg" autoplay loop><source src="media/videos/'+ pFile +'.webm" type="video/webm"/><source src="media/videos/'+ pFile +'.mp4" type="video/mp4"/><source src="media/videos/'+ pFile +'.ogv" type="video/ogg"/></video>');
+    $('.pieces').append('<video class="piece" id="l'+ pId +'" width="'+ pWidth +'" height="'+ pHeight +'" style="top: '+ pY +'px; left: '+ pX +'px;" preload="auto" poster="media/posters/'+ pFile +'.jpg" loop><source src="media/videos/'+ pFile +'.webm" type="video/webm"/><source src="media/videos/'+ pFile +'.mp4" type="video/mp4"/><source src="media/videos/'+ pFile +'.ogv" type="video/ogg"/></video>').hide().fadeIn(250);
   }
 
   for (var i = 0; i < pieces.length; i++) {
+    /*(function(i){
+
+        window.setTimeout(function(){
+          piece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY);
+        }, i * 100);
+
+    }(i));*/
     piece(pieces[i].pId, pieces[i].pFile, pieces[i].pWidth, pieces[i].pHeight, pieces[i].pX, pieces[i].pY);
   }
+
+  /*
+  $(".faded").each(function(i) {
+    $(this).delay(i * 400).fadeIn();
+  });
+  */
 
   $( '.piece' ).on( 'click', function() {
     $(window).scrollTo($(this), 750, {offset:{left: -xpos, top: -ypos }, easing:"easeOutCubic"});
     return false;
   });
+
+  (function($){
+    $.fn.onPlayground = function() {
+
+      return this.each(function() {
+        var $piece = $(this);
+
+        // Set the status of the play button icon
+        if($piece.is(':in-viewport')) {
+          $piece[0].play();
+          console.log('play');
+        } else {
+          $piece[0].pause();
+          console.log('pause');
+        }
+
+        /*if($piece.not(':in-viewport') && $piece[0].paused === false) {
+          $piece[0].pause();
+          console.log('pause');
+        }*/
+
+        return this;
+      });
+    };
+  })(jQuery);
+
+  setTimeout(function(){ $(".piece").onPlayground(); }, 251);
+
+  /*$(window).scroll(function(){
+      $(".piece").onPlayground();
+  });*/
+
+  $(window).on('scrollstop', function() {
+    console.log('End of scrolling')
+    $(".piece").onPlayground();
+  });
+
+  /*$('video')[0].pause( function(){ alert("aloha") });
+
+  $('video:in-viewport')[0].play();
+
+  $(window).scroll(function(){
+      $('video:in-viewport')[0].play();
+      $('video').not('video:in-viewport')[0].pause();
+  });*/
 
   /*$(".piece:not(:in-viewport)").each(function() {
     this[0].pause();
